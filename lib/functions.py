f9adf779f2b44e31c404f1a78f0054101be60d9e
+ <+>
2023-11-19 14:36:46 +0300

    completed assinnment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+   
 
 def greet(name):
-    pass
+    print(f"Hello,{name}!")
+greet("Angela")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello,{name}")
+greet_with_default("Tom")
 
 def add(num1, num2):
-    pass
+    print(num1+num2)
+add(10,20)
 
 def halve(number):
-    pass
+    print(number/2)
+halve(30)
