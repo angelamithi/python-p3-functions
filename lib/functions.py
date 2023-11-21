c7ff2dcf202bb23a9fb67edba4b81cfcc926438e
+ <+>
2023-11-21 14:27:04 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -5,17 +5,17 @@ def greet_programmer():
    
 
 def greet(name):
-    print(f"Hello,{name}!")
-greet("Angela")
+    print(f"Hello, {name}!")
+greet("Guido")
 
 def greet_with_default(name="programmer"):
-    print(f"Hello,{name}")
+    print(f"Hello, {name}!")
 greet_with_default("Tom")
 
 def add(num1, num2):
-    print(num1+num2)
+    return num1+num2
 add(10,20)
 
 def halve(number):
-    print(number/2)
+    return number/2
 halve(30)
